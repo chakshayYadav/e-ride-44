6d4a83cdc9cae8db850bf997f8943af7080d23aa
chakshayYadav <74128864+chakshayYadav@users.noreply.github.com>
2022-04-29 17:30:51 +0530

    Update config.js

--- e ride 4/config.js
+++ e ride 4/config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDxujqHwSNmA3ur4nBRFWqKhxBx47zoOYY",
+  authDomain: "e-ride-f8994.firebaseapp.com",
+  projectId: "e-ride-f8994",
+  storageBucket: "e-ride-f8994.appspot.com",
+  messagingSenderId: "1082651400126",
+  appId: "1:1082651400126:web:dc343fddf666e6a7908bef"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
